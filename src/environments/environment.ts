27392d3eb9536254d2e27baa7220f3a8ad5d8e81
valmas <valerio.massi@gmail.com>
2020-06-30 10:07:12 +0200

    new prod setting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const apiUrl = 'http://localhost:8080/';
+const apiUrl = 'http://79.8.14.12:10080/spstimesheet/';
 
 export const environment = {
   production: false,
@@ -10,8 +10,8 @@ export const environment = {
   //authenticationUrl : 'http://localhost:4000/users/authenticate',
   //usersUrl: 'http://localhost:4000/users',
 
-  authenticationUrl : 'http://localhost:8080/login.php',
-  usersUrl: 'http://localhost:8080/users/',
+  authenticationUrl : 'http://79.8.14.12:10080/spstimesheet/login.php',
+  usersUrl: 'http://79.8.14.12:10080/spstimesheet/users/',
   userCreation: apiUrl + '/user/createUser.php',
 };
 
