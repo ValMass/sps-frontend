675482f890714cff1ae9ceea11e40cc1add53d1c
valmas <valerio.massi@gmail.com>
2020-06-30 15:31:53 +0200

    new change to enviroment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  apiUrl: 'http://79.8.14.12:10080/spstimesheet/',
-  authenticationUrl : 'http://79.8.14.12:10080/spstimesheet/login.php',
+  apiUrl: 'https://79.8.14.12:10080/spstimesheet/',
+  authenticationUrl : 'https://79.8.14.12:10080/spstimesheet/login.php',
   usersUrl: 'http://79.8.14.12:10080/spstimesheet/users',
 };
